3d806c81c372b74385d267dbad748030dd004170
angeltg83 <angel.tigua83@gmail.com>
2020-12-18 16:55:06 -0500

    modificaciones para generar y autorizar el usuario desde el front

--- Controllers/AuthController.js
+++ Controllers/AuthController.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const bcrypt = require("bcrypt");
 
 const jwt = require("jsonwebtoken"),
   config = require("../config/config");
@@ -14,33 +15,41 @@ const connection = require("../config/datastore");
  * @since 2020.12.18
  * @returns {json} token
  */
+
 getAuth = async (req, res) => {
-  const db = await connection();
-  // console.log("req --- ", req);
-  const { username, contrasena } = req.body;
-  const persona = await db
-    .collection("persona")
-    .find({ "user.username": username, "user.password": contrasena })
-    .toArray();
+  try {
+    const db = await connection();
+    // console.log("req.query -- ", req.body);
+    const { username, contrasena } = req.body;
+    const persona = await db
+      .collection("persona")
+      .find({ "user.username": username })
+      .toArray();
 
-  console.log("persona find ", persona);
-  const { user } = persona[0];
-
-  if (username === user.username && contrasena === user.password) {
-    // console.log("ENTRO!!");
-    const payload = {
-      check: true,
-    };
-    const token = jwt.sign(payload, app.get("key"), {
-      expiresIn: "24d",
-    });
-    res.json({
-      status: true,
-      token,
-    });
-  } else {
-    res.json({ mensaje: "Usuario o contraseña incorrectos" });
+    const match = await bcrypt.compare(contrasena, persona[0].user.password);
+    if (!match) {
+      throw new Error({ err: "Usuario o clave no registrada" });
+    } else {
+      const token = jwt.sign(
+        {
+          check: true,
+        },
+        app.get("key"),
+        {
+          expiresIn: "24d",
+        }
+      );
+      console.log("token -->", token);
+      return res.json({
+        token,
+        persona,
+      });
+    }
+  } catch (err) {
+    // console.log("err 00 ",err)
+    res.status(500).send({ err, token: null });
   }
+
   // console.log("Local");
   // res.status(200).send({ msg: "mensaje desde controlador", status: true });
 };
--- routes.js
+++ routes.js
@@ -13,10 +13,16 @@ app.use(bodyParser.json());
 
 const UsuarioController = require("./Controllers/UsuarioController");
 const AuthController = require("./Controllers/AuthController");
+// const ExerciseController = require("./Controllers/ExerciseController");
 
-const ExerciseController = require("./Controllers/ExerciseController");
-
-app.options('*', cors())
+// app.options('*', cors())
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
 app.use(express.Router());
 
 const rutasProtegidas = express.Router();
