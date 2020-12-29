63d628c23686fe53cdbc904796e32acbf68081f1
angeltg83 <angel.tigua83@gmail.com>
2020-12-29 17:58:43 -0500

    chat v1

--- config/config.js
+++ config/config.js
@@ -1,3 +1,5 @@
 module.exports = {
-    key: "tWZVu+cBrLHE2LkqTAuFdNJKgDr8w5EYcSfEbj/h2bI=*"
-}
+  key: "tWZVu+cBrLHE2LkqTAuFdNJKgDr8w5EYcSfEbj/h2bI=*",
+  port: 7000,
+  NEW_CHAT_MESSAGE_EVENT: "newChatMessage",
+};
--- index.js
+++ index.js
@@ -1,27 +1,43 @@
-const express = require("express");
-// const config = require('./config/config')
-
-
-
 // const bodyParser = require("body-parser");
-const app = express();
+const app = require("express")();
+const routes = require("./routes");
 
+const { port, NEW_CHAT_MESSAGE_EVENT } = require("./config/config");
+
+app.use("/", routes);
+const http = require("http").createServer(app);
+const io = require("socket.io")(http, {
+  cors: {
+    origin: "*",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected!!");
+  const { roomId } = socket.handshake.query;
+  socket.join(roomId);
+
+  // Listen for new messages
+  socket.on(NEW_CHAT_MESSAGE_EVENT, (data) => {
+    io.in(roomId).emit(NEW_CHAT_MESSAGE_EVENT, data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log(`Client ${socket.id} diconnected`);
+    socket.leave(roomId);
+  });
+  
+});
+
+http.listen(port, () => {
+  console.info("starting server in port : ", port);
+});
+
+// const port = 7000;
+
+// Para ejecutar, en la raiz del proyecto
+// node index.js
 
 // app.use(bodyParser.urlencoded({ extended: false }));
 // app.use(bodyParser.json());
 // const UsuarioController = require('./Controllers/UsuarioController');
-
-const port = 7000;
-
-// Para ejecutar, en la raiz del proyecto 
-// node index.js
-
-const routes = require("./routes");
-
-// app.use(express.Router());
-
-app.use("/", routes);
-
-app.listen(port, () => {
-  console.info("starting server in port : ", port);
-});
